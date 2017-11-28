fd53b741c44d74f03ef11c524eaf7d934128ef60
Tobi Ajala <tobi-ajala@hotmail.com>
2017-11-28 08:28:57 +0000

    tidy

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
   render() {
 
     const { showing } = this.state;
+    const { end } = this.state;
 
     return (
       <div className="App">
@@ -18,36 +19,33 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
 
-        <div className="module">
-          <div className="heading">
-            <p className="module-title">Module Title</p>
-            <div style={{ display: this.state.showStore ? 'none' : 'block' }}> <p className="close">X</p> </div>
-          </div>
-          <p className="dummy-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur.
 
-            <div style={{ display: (showing ? 'none' : 'block') }}>
-              <p className="text-one">
-                <p className="text-title"> Details </p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud.
-              </p>
-              <p className="text-title"> More info </p>
+        <div className="module">
+          <p className="close" onClick={() => this.setState({ end: !end })}>X</p>
+          <div className="close-all" style={{ display: (end ? 'none' : 'block') }}>
+            <p className="module-title">Module Title</p>
+            <p className="dummy-text">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud.
-            </div>
-            <button className="show" onClick={() => this.setState({ showing: !showing })}>Show More</button>
-
-          </p>
+              quis nostrud <b>Exercitation</b> ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur.
+              <div style={{ display: (showing ? 'none' : 'block') }}>
+                <p className="text-one">
+                  <p className="text-title"> Details </p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud.
+                  <p className="text-title"> More info </p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud.
+                </p>
+              </div>
+              <button className="show" onClick={() => this.setState({ showing: !showing })}>Show More</button>
+            </p>
+          </div>
         </div>
-
-
       </div>
     );
   }
